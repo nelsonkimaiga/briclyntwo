fc2755417cf838d062b1e2909d62e1cc33eca754
joseph muli <jayjaymuli00@gmail.com>
2016-07-20 19:23:53 +0300

    listing model added

--- briclyn/models.py
+++ briclyn/models.py
@@ -7,6 +7,7 @@ from allauth.account.models import EmailAddress
 
 class UserProfile(models.Model):
 	user = models.OneToOneField(User, related_name='profile')
+	phone = models.CharField(max_length=100, unique=False, blank=True, null=True)
 
 	def __unicode__(self):
 		return "{}'s profile".format(self.user.username)
@@ -21,3 +22,23 @@ class UserProfile(models.Model):
 				return result[0].verified
 		return False
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
+
+
+class listing(models.Model):
+	property_type = models.CharField(max_length=100, unique=False, blank=True)
+	customer_type = models.CharField(max_length=100, unique=False, blank=True)
+	title = models.CharField(max_length=250, unique=False, blank=True)
+	description = models.CharField(max_length=250, unique=False, blank=False)
+	baths = models.IntegerField(default=0)
+	area = models.IntegerField(default=0)
+	cost = models.IntegerField(default=0)
+	price = models.DecimalField(max_digits=9,decimal_places=2, default=0)
+	city= models.CharField(max_length=100, unique=False,blank=True)
+	location= models.CharField(max_length=100, unique=False,blank=True)
+
+	def __unicode__(self):
+		return '%s' %self.title
+
+
+	def __str__(self):
+		return self.title
