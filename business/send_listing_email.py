7a0413c5a8b481dec1dda4ea2aa8065ca3494ada
joseph muli <jayjaymuli00@gmail.com>
2016-07-31 20:18:05 +0300

    listing email send

---
+++ business/send_listing_email.py
@@ -0,0 +1,26 @@
+# from adlink.models import clerk_auth
+from django.contrib.auth.models import User
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
+class SendAdEmail():
+	def run (data):
+		new_listing_data = data.get('new_listing_data')
+		user = request.user
+		
+		subject ="Your Listing was created succesfully"
+		subject, from_email, to = subject, settings.EMAIL_HOST_USER, user.email
+		text_content = 'Your Briclyn Listing has been added'
+		html_content = "<p>This is your <strong>adlink</strong>"
+		msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+		msg.attach_alternative(html_content, "text/html")
+		msg.send()
+
+
+
+
+
+
+		
+		
+		
+	
